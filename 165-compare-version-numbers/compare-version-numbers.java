2f55186cc875f06e4047701aa75cf3a5a12ef049
parulkaushal06 <parul2958.beai24@chitkara.edu.in>
2026-07-20 23:27:20 +0530

    Time: 1 ms (77.63%) | Memory: 43.1 MB (25.57%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -15,7 +15,7 @@ class Solution {
             if(num1<num2){
                 return -1;
             }
-            if(num1>num2){
+            if(num2<num1){
                 return 1;
             }
         }
