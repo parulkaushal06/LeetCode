5a779c48a88503aa72fd6edb255c7a3f6be1d601
parulkaushal06 <parul2958.beai24@chitkara.edu.in>
2026-07-25 14:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (68.51%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -14,7 +14,7 @@ class Solution {
         ListNode prev = null;
         while(curr != null){
             ListNode next = curr.next ;
-            curr.next = prev;
+            curr.next = prev ;
             prev = curr ;
             curr = next ;
         }
