bacba381e11dbc9b038cdf2bba93b0c2d1473dcf
parulkaushal06 <kaushalparul06@gmail.com>
2026-08-14 14:39:32 +0530

    Time: 1 ms (96.59%) | Memory: 42.8 MB (18.28%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -3,12 +3,12 @@ class Solution {
         if(n<=0){
             return false;
         }
-            if(n==1){
+        if(n==1){
             return true ;
-            }
-            if(n%2!=0){
-                return false ;
-            }
+        }
+        if(n%2!=0){
+            return false ;
+        }
         return isPowerOfTwo(n/2);
     }
 }
