9c1d03462e0e186ff163a1944e07f54cfed9f70d
parulkaushal06 <kaushalparul06@gmail.com>
2026-08-25 08:15:53 +0530

    Time: 4 ms (16.89%) | Memory: 45.2 MB (42.29%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,21 +1,20 @@
 class Solution {
-    public List<List<Integer>> printSubsets(int i, int[]nums, List<Integer>li, List<List<Integer>> list){
-        if(i==nums.length){
+    public List<List<Integer>> subsetsDup(List<List<Integer>>list,int i, List<Integer> li,int[] nums) {
+        if(i>=nums.length){
             list.add(new ArrayList<>(li));
-            return list;
+            return list ;
         }
         li.add(nums[i]);
-        printSubsets(i+1, nums, li, list);
+        subsetsDup(list, i+1, li, nums);
         li.remove(li.size()-1);
-        printSubsets(i+1, nums, li, list);
-        return list ;
+        subsetsDup(list, i+1, li, nums);
+        return list;
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
-        printSubsets(0, nums, new ArrayList<>(), list);
-        Set<List<Integer>> set = new LinkedHashSet<>(list);
-        return new ArrayList<>(set);
-
+        subsetsDup(list, 0, new ArrayList<>(), nums);
+        Set<List<Integer>> result = new LinkedHashSet<>(list);
+        return new ArrayList<>(result);
     }
 }
