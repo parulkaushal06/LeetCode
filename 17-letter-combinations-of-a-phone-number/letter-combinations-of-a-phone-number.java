d4e10578cdb6d0319c866560eeb062c28c0eeb55
parulkaushal06 <kaushalparul06@gmail.com>
2026-08-29 17:31:43 +0530

    Time: 3 ms (32.60%) | Memory: 49.2 MB (10.93%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,6 +1,7 @@
 class Solution {
-    public void answer(String digits, String current, List<String> list, String[] mapping, int index ){
-        if(index == digits.length()){
+    public void answer(String digits, List<String> list, String current,
+    int index, String[] mapping){
+        if(index==digits.length()){
             list.add(current);
             return ;
         }
@@ -8,30 +9,28 @@ class Solution {
         String letters = mapping[digit];
         for(int i=0; i<letters.length(); i++){
             current += letters.charAt(i);
-            answer(digits, current, list, mapping, index+1);
-            current = current.substring(0, current.length()-1);
+            answer(digits, list, current, index+1, mapping);
+            current = current.substring(0,current.length()-1);
         }
-        return ;
     }
     public List<String> letterCombinations(String digits) {
         List<String> list = new ArrayList<>();
-         if (digits.length() == 0) {
-            return list;
+        if(digits.length()==0){
+            return list ;
         }
-        String [] mapping = 
-        {"",
-         "",
-        "abc",
-        "def",
-        "ghi",
-        "jkl",
-        "mno",
-        "pqrs",
-        "tuv",
-        "wxyz"
+        String [] mapping = {
+            "",
+            "",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
         };
-        
-        answer(digits, "",list, mapping, 0);
+        answer(digits, list, "", 0, mapping);
         return list ;
     }
 }
