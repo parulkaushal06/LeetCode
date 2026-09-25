723ba0c9b13eb18fbe08b326309e294bc1441471
parulkaushal06 <kaushalparul06@gmail.com>
2026-09-25 13:34:51 +0530

    Time: 4 ms (73.46%) | Memory: 44.3 MB (99.37%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,33 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
-        int[] answer = new int[nums1.length];
-
-        // Initially all answers are -1
-        Arrays.fill(answer, -1);
-
         Stack<Integer> st = new Stack<>();
-
-        for (int i = 0; i < nums2.length; i++) {
-
-            while (!st.isEmpty() && nums2[i] > nums2[st.peek()]) {
-
-                int index = st.pop();
-
-                // Find this value in nums1
-                for (int j = 0; j < nums1.length; j++) {
-                    if (nums1[j] == nums2[index]) {
-                        answer[j] = nums2[i];
+        int n = nums1.length;
+        int[] answers = new int[n];
+        for(int i=0; i<n; i++){
+            answers[i] = -1;
+        }
+        for(int i=0; i<nums2.length; i++){
+            while(!st.isEmpty() && nums2[i]>nums2[st.peek()]){
+                int prev = st.pop();
+                for(int j=0; j<n; j++){
+                    if(nums1[j]==nums2[prev]){
+                        answers[j] = nums2[i];
                         break;
                     }
                 }
             }
-
             st.push(i);
         }
-
-        return answer;
+        return answers ;
     }
 }
